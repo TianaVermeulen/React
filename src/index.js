22aaab6824f6e7293b2601d61fba00aa8c67546c
Tiana O’Driscoll <tianaodriscoll@Tianas-MacBook-Pro.local>
2021-10-25 13:59:27 +0200

    Challenge 4 - JSX attributes and and styling React elements

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const name = "Tiana";
-const currentYear = new Date().getFullYear();
+const image = "https://picsum.photos/200/300";
 
 ReactDOM.render(<div>
-<p>Created by {name}</p>
-<p>Copyright {currentYear}</p>
+  <h1 className="heading" contentEditable="true" spellCheck="false">My Favourite Foods</h1>
+  <div>
+    <img src="https://picsum.photos/200"></img>
+    <img src={image}></img>
+  </div>
 </div>
-, document.getElementById("root"));
+  , document.getElementById("root"));
