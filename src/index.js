7c2a1b17fc3e1417f7a50d9962518fb8a924135e
Tiana O’Driscoll <tianaodriscoll@Tianas-MacBook-Pro.local>
2021-10-25 14:48:57 +0200

    React Styling Practice with inline stuling

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const image = "https://picsum.photos/200/300";
+const date = new Date();
+const currentTime = date.getHours();
+
+let greeting;
+
+const customStyle = {
+  color: ""
+}
+
+if (currentTime < 12) {
+  greeting = "Good Morning";
+  customStyle.color = "red";
+} else if (currentTime < 18) {
+  greeting = "Good Afternoon";
+  customStyle.color = "green";
+} else {
+  greeting = "Good Evening";
+  customStyle.color = "blue";
+}
 
 ReactDOM.render(<div>
-  <h1 className="heading" contentEditable="true" spellCheck="false">My Favourite Foods</h1>
-  <div>
-    <img src="https://picsum.photos/200"></img>
-    <img src={image}></img>
-  </div>
+  <h1 className="heading" style={customStyle}>{greeting}</h1>
 </div>
   , document.getElementById("root"));
