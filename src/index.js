4b6231bf5a31fc0a3eaf908de976ee7db5424138
Tiana O’Driscoll <tianaodriscoll@Tianas-MacBook-Pro.local>
2021-10-25 13:07:36 +0200

    adding space between two js elements

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const name = "Tiana"
+const fname = "Tiana"
+const lname = "Vermeulen"
+
+//can also add space by using <h1>My name is {fname+ " " +lname}
 
 ReactDOM.render(<div>
-<h1>My name is {name}</h1>
+<h1>My name is {fname} {lname}</h1>
 <p>My lucky number is 8.</p>
 </div>
 , document.getElementById("root"));
