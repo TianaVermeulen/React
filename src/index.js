6c04846f4dd7b6d37e8785b533b5db4cc10b30f3
Tiana O’Driscoll <tianaodriscoll@Tianas-MacBook-Pro.local>
2021-10-25 12:56:37 +0200

    Challenge 1 - JSX code practice by adding h1 and list of items to render

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<h1>Hello world!</h1>, document.getElementById("root"));
+ReactDOM.render(<div>
+<h1>My Favourite Foods</h1>
+<ul>
+  <li>Burgers</li>
+  <li>Pizza</li>
+  <li>Chocolate</li>
+</ul>
+</div>
+, document.getElementById("root"));
