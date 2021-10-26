0ce00b59c8b40cd3d203005af94e2aed71c46a62
Tiana O’Driscoll <tianaodriscoll@Tianas-MacBook-Pro.local>
2021-10-26 13:03:09 +0200

    Introduction to Import, Export and Modules

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
+import React from "react";
+import ReactDOM from "react-dom";
+import pi, { doublePi, triplePi }  from "./components/Math";
+// a wild card can also be used for import - import * as pi from "./components/Math"
+// but you lose the benefit of using the default option
 
 ReactDOM.render(
-  <App />,
+  <ul>
+    <li>{pi}</li>
+    <li>{doublePi()}</li>
+    <li>{triplePi()}</li>
+  </ul>,
   document.getElementById("root")
 );
