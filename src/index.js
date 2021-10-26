fcc516f9cf97b63bf6b34b0b43573dbfbc97b33b
Tiana O’Driscoll <tianaodriscoll@Tianas-MacBook-Pro.local>
2021-10-26 12:34:35 +0200

    Challenge 5 - breaking code up in react components to make code easier to work on in future

--- src/index.js
+++ src/index.js
@@ -1,27 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/App';
 
-const date = new Date();
-const currentTime = date.getHours();
-
-let greeting;
-
-const customStyle = {
-  color: ""
-}
-
-if (currentTime < 12) {
-  greeting = "Good Morning";
-  customStyle.color = "red";
-} else if (currentTime < 18) {
-  greeting = "Good Afternoon";
-  customStyle.color = "green";
-} else {
-  greeting = "Good Evening";
-  customStyle.color = "blue";
-}
-
-ReactDOM.render(<div>
-  <h1 className="heading" style={customStyle}>{greeting}</h1>
-</div>
-  , document.getElementById("root"));
+ReactDOM.render(
+  <App />,
+  document.getElementById("root")
+);
