53f4793be546d4ffa1aaab2b766187c0a857d4f6
Tiana O’Driscoll <tianaodriscoll@Tianas-MacBook-Pro.local>
2021-10-26 13:09:29 +0200

    Challenge 6 - Import, Export and Modules

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import pi, { doublePi, triplePi }  from "./components/Math";
-// a wild card can also be used for import - import * as pi from "./components/Math"
-// but you lose the benefit of using the default option
+import {add, multiply, subtract, divide} from "./components/Calculator"
+
+//Import the add, multiply, subtract and divide functions
+//from the calculator.js file.
+//If successful, your website should look the same as the Final.png
 
 ReactDOM.render(
   <ul>
-    <li>{pi}</li>
-    <li>{doublePi()}</li>
-    <li>{triplePi()}</li>
+    <li>{add(1, 2)}</li>
+    <li>{multiply(2, 3)}</li>
+    <li>{subtract(7, 2)}</li>
+    <li>{divide(5, 2)}</li>
   </ul>,
   document.getElementById("root")
 );
